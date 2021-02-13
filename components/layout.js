0fdf39ae027718d0fb23987733094ab183c55ee6
Angramme <kacpero376@gmail.com>
2021-02-13 19:44:51 +0100

    added cookie consent

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,21 @@
 import { themify } from "../lib/darkify"
 import styles_raw from "./layout.module.sass"
+import CookieConsent from "react-cookie-consent";
 
 export default function Layout({children, style}) {
     const styles = themify(styles_raw);
     return <div className={styles.layout} style={style} lang="en-GB">
         {children}
+
+        <CookieConsent
+            location="bottom"
+            buttonText="I agree!"
+            cookieName="cookie_consent_agreed"
+            style={{fontSize: "13px"}}
+            buttonStyle={styles.cookie_consent_btn}
+            expires={150}
+            >
+            This website uses cookies to enhance the user experience (darkmode settings).
+        </CookieConsent>
     </div>
 }
