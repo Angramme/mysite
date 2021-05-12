6b9d6d7fffa76bfb18290a26ebeeb6c711b1bea2
Angramme <kacpero376@gmail.com>
2021-05-12 21:29:53 +0200

    fixed tooltip errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
 import '../styles/globals.sass'
 import withDarkMode from 'next-dark-mode'
 
+import ReactTooltip from "react-tooltip"
+import react from 'react'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  // Tooltip stuff...
+  const [isTooltipVisible, setTooltipVisibility] = react.useState(false);
+  react.useEffect(() => {
+    setTooltipVisibility(true);
+  }, []);
+
+  return <div>
+      <Component {...pageProps} />
+      {isTooltipVisible && <ReactTooltip 
+        effect="solid"
+        place="left"
+        backgroundColor="gray"
+        color="white"
+        borderColor="white"/>}
+    </div>
 }
 
 export default withDarkMode(MyApp)
